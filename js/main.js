f1255193b3e783b537ff247b3d55a66187f29409
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-01-09 15:59:41 +0300

    add destinations section

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// $(document).ready(function () {
+//   $(".owl-carousel").owlCarousel({
+
+//   });
+// });
+
+const owl = $(".owl-carousel");
+owl.owlCarousel({
+  center: true,
+  loop: true,
+  margin: 30,
+  startPosition: 1,
+  items: 3,
+});
+
+// Go to the previous item
+$(".slider__btn--prev").click(function () {
+  // With optional speed parameter
+  // Parameters has to be in square bracket '[]'
+  owl.trigger("prev.owl.carousel");
+});
+
+// Go to the next item
+$(".slider__btn--next").click(function () {
+  owl.trigger("next.owl.carousel");
+});
